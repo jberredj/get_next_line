90331603d96c5086d47971695918b40ab96fb7bf
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-12-22 01:14:46 +0100

    Last commit before peer evaluation.

---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/17 16:58:32 by jberredj          #+#    #+#             */
+/*   Updated: 2020/12/22 00:25:58 by jberredj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <stdlib.h>
+# define COPY 1
+# define NOCOPY 0
+
+int		ft_gnljoin(char **s1, char *s2);
+size_t	ft_strlcpy(char *dest, char *src, size_t n);
+int		get_next_line(int fd, char **line);
+void	ft_bzero(void *s, size_t n);
+#endif
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+#endif
+
+#ifndef FD_MAX
+# define FD_MAX 65536
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/17 17:13:44 by jberredj          #+#    #+#             */
+/*   Updated: 2020/12/22 00:51:57 by jberredj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static size_t	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str++)
+		i++;
+	return (i);
+}
+
+static size_t	ft_strlcat(char *dest, char *src, size_t size)
+{
+	size_t	i;
+	size_t	src_len;
+	size_t	dest_len;
+
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+	if (size <= dest_len)
+		return (src_len + size);
+	dest = &dest[dest_len];
+	i = 0;
+	while (i < (size - (dest_len) - 1) && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest_len + src_len);
+}
+
+void			ft_bzero(void *s, size_t n)
+{
+	unsigned char *uchar_s;
+
+	uchar_s = (unsigned char*)s;
+	while (n-- > 0)
+		*uchar_s++ = '\0';
+}
+
+size_t			ft_strlcpy(char *dest, char *src, size_t n)
+{
+	size_t	i;
+	int		str_len;
+
+	str_len = 0;
+	while (src[str_len] != '\0')
+	{
+		str_len++;
+	}
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (str_len);
+}
+
+int				ft_gnljoin(char **s1, char *s2)
+{
+	char	*str;
+	size_t	str_len;
+
+	if (*s1 == NULL)
+	{
+		if (!(*s1 = (char*)malloc(sizeof(char))))
+			return (-1);
+		ft_bzero(s1, 1);
+	}
+	str_len = ft_strlen(*s1) + ft_strlen(s2) + 1;
+	if (!(str = (char*)malloc(sizeof(char) * str_len)))
+		return (-1);
+	ft_strlcpy(str, *s1, str_len);
+	ft_strlcat(str, s2, str_len);
+	if (*s1 != NULL)
+		free(*s1);
+	*s1 = str;
+	return (0);
+}
